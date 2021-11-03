e73f88f122493a5ac6c3ced92d70d11eee088798
hariose <67162120+hariose@users.noreply.github.com>
2021-11-03 22:59:33 +0900

    1주차 튜토리얼
    BasicsCodelab2 이름이라는 폴더로 파일 옮겨서 진행

---
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/rabbitfactory/basicscodelab2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.rabbitfactory.basicscodelab2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.rabbitfactory.basicscodelab2.ui.theme.BasicsCodelab2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Text(text = msg.author)
+    Text(text = msg.body)
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
+}
+
